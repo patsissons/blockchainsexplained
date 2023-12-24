0e5b6a011b56fc6bfcf4e8aaf9063503405f42ff
Pat Sissons <patricksissons@gmail.com>
2023-12-24 11:13:11 -0700

    adding topic footer
    - also refactoring links to a component

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import '$lib/styles/app.css'
+  import Link from './link.svelte'
 </script>
 
 <div class="flex flex-col gap-2 min-h-screen">
@@ -11,14 +12,10 @@
 
   <footer class="bg-neutral-300 text-center px-2 py-0.5">
     <p>
-      This is an
-      <a
-        class="mx-0.5 text-cyan-700 hover:text-cyan-600 focus:ring ring-cyan-700/25 outline-none rounded font-medium"
+      This is an <Link
         href="https://github.com/patsissons/blockchainsexplained"
-        rel="noopener noreferrer"
-        target="_blank">open source project on GitHub</a
-      >
-      aimed to inform without bias on all aspects of blockchains.
+        external>open source project on GitHub</Link
+      > aimed to inform without bias on all aspects of blockchains.
     </p>
   </footer>
 </div>
---
+++ src/routes/link.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  export let href: string
+  export let external = false
+</script>
+
+<a
+  class="px-0.5 rounded font-medium text-cyan-700 hover:text-cyan-600 focus:ring ring-cyan-700/25 outline-none"
+  {href}
+  rel={external ? 'noopener noreferrer' : undefined}
+  target={external ? '_blank' : undefined}><slot /></a
+>
